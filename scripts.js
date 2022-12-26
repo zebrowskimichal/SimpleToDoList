b1c5e0e0aed4172e3e299f93e4c5aad822ce9050
zebrowskimichal <zebrowski.michal02@gmail.com>
2022-12-26 20:36:56 +0100

    Added script that deleted clicked tasks

--- scripts.js
+++ scripts.js
@@ -29,5 +29,24 @@ function add_entry(data) {
 		.then(function (response) {
 			console.log(response);
 			load_tasks();
+			new_todo.value = "";
 		});
+}
+function delete_entry(data, rowId) {
+	rowId.classList.add("deleted");
+	setTimeout(() => {
+		fetch("delete.php?id=" + data, {
+			method: "get",
+		})
+			.then(function (response) {
+				if (response.status >= 200 && response.status < 300) {
+					return response.text();
+				}
+				throw new Error(response.statusText);
+			})
+			.then(function (response) {
+				console.log(response);
+				rowId.style.display = "none";
+			});
+	}, 3000);
 }
